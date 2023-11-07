63a3f2ab86eb367637de671162159395f1d3a404
BG8LGP <130383549+BG8LGP@users.noreply.github.com>
2023-11-08 00:40:42 +0800

    Update welcome.c

--- ui/welcome.c
+++ ui/welcome.c
@@ -34,7 +34,7 @@ void UI_DisplayReleaseKeys(void)
 
 	//UI_PrintString("RELEASE", 0, 127, 1, 10);
 	//UI_PrintString("ALL KEYS", 0, 127, 3, 10);
-	UI_PrintString("_test", 0, 127, 1, 10);
+	UI_PrintString('_', 0, 127, 1, 10);
 
 	ST7565_BlitStatusLine();  // blank status line
 	ST7565_BlitFullScreen();
