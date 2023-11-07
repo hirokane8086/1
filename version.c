4c056a405143ad8fd35800403c9e358cb5227bdd
BG8LGP <130383549+BG8LGP@users.noreply.github.com>
2023-11-08 00:44:13 +0800

    Update version.c

--- version.c
+++ version.c
@@ -4,13 +4,13 @@
 #ifdef GIT_HASH
 	#define VER     GIT_HASH
 #else
-	#define VER     "231004"
+	#define VER     "0.0.1-ahpla"
 #endif
 
 #ifndef ONE_OF_ELEVEN_VER
 	const char Version[]      = "OEFW-"VER;
 	const char UART_Version[] = "UV-K5 Firmware, Open Edition, OEFW-"VER"\r\n";
 #else
-	const char Version[]      = "1o11+fagci-"VER;
-	const char UART_Version[] = "UV-K5 Firmware, Open Edition, 1o11+fagci-"VER"\r\n";
+	const char Version[]      = "BG8LGP-"VER;
+	const char UART_Version[] = "UV-K5 Firmware, Open Edition, bg8lgp-"VER" from 1o11 and egzumer\r\n";
 #endif
