8c149c37e3273633efbfdd257462cd59c5f1bba7
BG8LGP <130383549+BG8LGP@users.noreply.github.com>
2023-11-09 00:14:03 +0800

    Update helper.c
    好像走错路了

--- ui/helper.c
+++ ui/helper.c
@@ -75,9 +75,9 @@ void UI_PrintString(const char *pString, uint8_t Start, uint8_t End, uint8_t Lin
 
 	for (i = 0; i < Length; i++)
 	{
+		const unsigned int ofs   = (unsigned int)Start + (i * Width);
 		if (pString[i] > ' ' && pString[i] < 127)
 		{
-			const unsigned int ofs   = (unsigned int)Start + (i * Width);
 			const unsigned int index = pString[i] - ' ' - 1;
 			memmove(gFrameBuffer[Line + 0] + ofs, &gFontBig[index][0], 7);
 			memmove(gFrameBuffer[Line + 1] + ofs, &gFontBig[index][7], 7);
@@ -85,12 +85,12 @@ void UI_PrintString(const char *pString, uint8_t Start, uint8_t End, uint8_t Lin
 		else
 		if (pString[i] > 127)
 		{
-			const unsigned int cnofs   = (unsigned int)Start + (i * Width/2);
 			for (j = 0; j< strlen(CNList)/3; j++)
 				if (pString[i]==CNList[3*j] && pString[i+1]==CNList[3*j+1] && pString[i+2]==CNList[3*j+2])
 				{
-					memmove(gFrameBuffer[Line + 0] + cnofs, &CNFont14[j][0], 14);
-					memmove(gFrameBuffer[Line + 1] + cnofs, &CNFont14[j][14], 14);
+					memmove(gFrameBuffer[Line + 0] + ofs, &CNFont14[j][0], 14);
+					memmove(gFrameBuffer[Line + 1] + ofs, &CNFont14[j][14], 14);
+					i+=2;
 					break;
 				}
 		}
