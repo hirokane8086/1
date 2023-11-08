052ec445105f629d83674501c55a65341ef4b893
BG8LGP <130383549+BG8LGP@users.noreply.github.com>
2023-11-08 17:27:42 +0800

    update

--- ui/menu.c
+++ ui/menu.c
@@ -503,6 +503,7 @@ void UI_DisplayMenu(void)
 		#ifdef ENABLE_AUDIO_BAR
 			case MENU_MIC_BAR:
 				strcpy(String, gSubMenu_OFF_ON[gSubMenuSelection]);
+				String[strlen(gSubMenu_OFF_ON[gSubMenuSelection])] = '\0'
 				break;
 		#endif
 
