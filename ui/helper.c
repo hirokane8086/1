dbd4465b4747c4351f89ac26005a43b4bc06e3b4
BG8LGP <130383549+BG8LGP@users.noreply.github.com>
2023-11-09 14:52:24 +0800

    Update helper.c

--- ui/helper.c
+++ ui/helper.c
@@ -95,7 +95,6 @@ void UI_PrintString(const char *pString, uint8_t Start, uint8_t End, uint8_t Lin
 				{
 					memmove(gFrameBuffer[Line + 0] + ofs, &CNFont14[j][0], 14);
 					memmove(gFrameBuffer[Line + 1] + ofs, &CNFont14[j][14], 14);
-					i+=1;
 					break;
 				}
 		}
