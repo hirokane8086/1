fc3efc0288f08ee8be2100a282b30651f29418d3
BG8LGP <130383549+BG8LGP@users.noreply.github.com>
2023-11-09 16:25:40 +0800

    Add files via upload

--- ui/helper.c
+++ ui/helper.c
@@ -66,9 +66,9 @@ void UI_GenerateChannelStringEx(char *pString, const bool bShowPrefix, const uin
 
 void UI_PrintString(const char *pString, uint8_t Start, uint8_t End, uint8_t Line, uint8_t Width)
 {
-	size_t i, j, ofs_fix;
+	size_t i, j;
 	size_t Length = strlen(pString);
-
+	size_t ofs_fix = 0;
 
 	if (End > Start)
 		Start += (((End - Start) - (Length * Width)) + 1) / 2;
