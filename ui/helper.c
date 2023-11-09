e236f8757abc07ab532c85ab872aa30ae9813d2a
BG8LGP <130383549+BG8LGP@users.noreply.github.com>
2023-11-09 10:33:32 +0800

    Add files via upload

--- ui/helper.c
+++ ui/helper.c
@@ -68,7 +68,6 @@ void UI_PrintString(const char *pString, uint8_t Start, uint8_t End, uint8_t Lin
 {
 	size_t i, j;
 	size_t Length = strlen(pString);
-	unsigned int ofs;
 
 
 	if (End > Start)
@@ -76,10 +75,7 @@ void UI_PrintString(const char *pString, uint8_t Start, uint8_t End, uint8_t Lin
 
 	for (i = 0; i < Length; i++)
 	{
-		if (i !=0 && pString[i] > 127)
-			ofs = (unsigned int)Start + ((i-2) * Width);
-		else
-			ofs = (unsigned int)Start + (i * Width);
+		const unsigned int ofs   = (unsigned int)Start + (i * Width);
 		if (pString[i] > ' ' && pString[i] < 127)
 		{
 			const unsigned int index = pString[i] - ' ' - 1;
@@ -89,14 +85,20 @@ void UI_PrintString(const char *pString, uint8_t Start, uint8_t End, uint8_t Lin
 		else
 		if (pString[i] > 127)
 		{
-			for (j = 0; j< strlen(CNList)/3; j++)
-				if (pString[i]==CNList[3*j] && pString[i+1]==CNList[3*j+1] && pString[i+2]==CNList[3*j+2])
-				{
-					memmove(gFrameBuffer[Line + 0] + ofs, &CNFont14[j][0], 14);
-					memmove(gFrameBuffer[Line + 1] + ofs, &CNFont14[j][14], 14);
-					break;
-				}
-			i+=2;
+			if (i != 0)
+				const unsigned int cnofs   = (unsigned int)Start + (i-1 * Width);
+			else
+			{
+				const unsigned int cnofs   = (unsigned int)Start + (i * Width);
+				for (j = 0; j< strlen(CNList)/3; j++)
+					if (pString[i]==CNList[3*j] && pString[i+1]==CNList[3*j+1] && pString[i+2]==CNList[3*j+2])
+					{
+						memmove(gFrameBuffer[Line + 0] + cnofs, &CNFont14[j][0], 14);
+						memmove(gFrameBuffer[Line + 1] + cnofs, &CNFont14[j][14], 14);
+						break;
+					}
+				i+=2;
+			}
 		}
 	}
 }
