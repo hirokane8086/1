6b918f42a89fc63655fb6011afee5ec35dce12df
BG8LGP <130383549+BG8LGP@users.noreply.github.com>
2023-11-09 15:19:17 +0800

    Add files via upload

--- ui/helper.c
+++ ui/helper.c
@@ -78,13 +78,10 @@ void UI_PrintString(const char *pString, uint8_t Start, uint8_t End, uint8_t Lin
 
 	for (i = 0; i < Length; i++)
 	{
-		if (i != 0 && pString[i-1] > 127)
-			ofs = (unsigned int)Start + (i-1 * Width);
-		else
-			ofs = (unsigned int)Start + (i * Width);
-
+		ofs = (unsigned int)Start + (i * Width);
 		if (pString[i] > ' ' && pString[i] < 127)
 		{
+
 			const unsigned int index = pString[i] - ' ' - 1;
 			memmove(gFrameBuffer[Line + 0] + ofs, &gFontBig[index][0], 7);
 			memmove(gFrameBuffer[Line + 1] + ofs, &gFontBig[index][7], 7);
@@ -99,6 +96,7 @@ void UI_PrintString(const char *pString, uint8_t Start, uint8_t End, uint8_t Lin
 					memmove(gFrameBuffer[Line + 0] + ofs, &CNFont14[j][0], 14);
 					memmove(gFrameBuffer[Line + 1] + ofs, &CNFont14[j][14], 14);
 					i+=2;
+					ofs = (unsigned int)Start + (i * Width);
 					break;
 				}
 		}
