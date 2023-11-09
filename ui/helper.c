cd42c30fda29f719c0cc4b14674707c2ef03fa6c
BG8LGP <130383549+BG8LGP@users.noreply.github.com>
2023-11-09 14:23:12 +0800

    update

--- ui/helper.c
+++ ui/helper.c
@@ -95,12 +95,10 @@ void UI_PrintString(const char *pString, uint8_t Start, uint8_t End, uint8_t Lin
 				{
 					memmove(gFrameBuffer[Line + 0] + ofs, &CNFont14[j][0], 14);
 					memmove(gFrameBuffer[Line + 1] + ofs, &CNFont14[j][14], 14);
+					i+=2;
+					ofs = (unsigned int)Start + (i * Width);
 					break;
 				}
-			i+=2;
-			// ofs = (unsigned int)Start + ((i+1-1) * Width);
-			if (pString[i+1] > 127)
-				ofs = (unsigned int)Start + ( i * Width);
 		}
 	}
 }
